cb4ee188497d25b79c64762f948e848d38cf0db2
dmdom <dmdom@192.168.0.136>
2020-10-20 11:00:39 +0200

    First commit

---
+++ kred_01/src/com/jsfcourse/calc/KredytBB.java
@@ -0,0 +1,88 @@
+package com.jsfcourse.calc;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+//import javax.enterprise.context.SessionScoped;
+//import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+@Named
+@RequestScoped
+//@SessionScoped
+public class KredytBB {
+	private String x;
+	private String y;
+	private String z;
+	private Double result;
+
+	@Inject
+	FacesContext ctx;
+
+	public String getX() {
+		return x;
+	}
+
+	public void setX(String x) {
+		this.x = x;
+	}
+
+	public String getY() {
+		return y;
+	}
+
+	public void setY(String y) {
+		this.y = y;
+	}
+
+	public Double getResult() {
+		return result;
+	}
+
+	public void setResult(Double result) {
+		this.result = result;
+	}
+
+	
+	public String getZ() {
+		return z;
+	}
+
+	public void setZ(String z) {
+		this.z = z;
+	}
+	
+	public boolean doTheMath() {
+		try {
+			double x = Double.parseDouble(this.x);
+			double y = Double.parseDouble(this.y);
+			double z = Double.parseDouble(this.z);
+			
+			z=z/100;
+			result = (x+(x*z))/y;
+
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
+			return true;
+		} catch (Exception e) {
+			ctx.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "B³¹d podczas przetwarzania parametrów", null));
+			return false;
+		}
+	}
+ 
+	public String calc() {
+		
+		if (doTheMath()) {
+			return "showresult";
+		}
+		return null;
+		
+		
+	}
+
+
+	
+
+	
+}
